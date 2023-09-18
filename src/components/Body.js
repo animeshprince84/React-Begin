7a2f4b8812d29c65c1ec2a3f4b427d5560479fe5
animeshprince84 <animeshprince84@gmail.com>
2023-09-18 14:59:27 +0530

    Called swiggy restaurant APi and implemented search

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,48 @@
 import RestaurantCard from "./RestaurantCard";
-import {resList} from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () =>{
 
-    const [listOfRestaurants,setListOfRestaurant] = useState(resList);
+    const [listOfRestaurants,setListOfRestaurant] = useState([]);
+    const [filteredRestaurant,setFilteredRestaurant] = useState([]);
 
-    return (
+    const [searchText,setSearchText] = useState("");
+
+    useEffect( ()=>{
+        fetchData();
+    },[]);
+
+    const fetchData = async ()=> {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+
+        const json = await data.json();
+
+        setListOfRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    return listOfRestaurants.length == 0 ? <Shimmer/> :(
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText} 
+                    onChange={(e)=>{
+                        setSearchText(e.target.value);
+                    }}/>
+                    <button onClick={()=>{
+
+                        const filteredRestaurant = listOfRestaurants.filter((res)=>
+                            res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                        );
+                        setFilteredRestaurant(filteredRestaurant);
+
+                    }}>Search</button>
+                </div>
                 <button className="filter-btn" 
                 onClick={()=>{
                     const filteredList = listOfRestaurants.filter(
-                        (res) => res.data.avgRating > 4
+                        (res) => res.info.avgRating > 4
                     );
                     setListOfRestaurant(filteredList);
                 }}>
@@ -21,8 +51,8 @@ const Body = () =>{
             </div>
             <div className="res-container">
                 {
-                    listOfRestaurants.map((restaurant)=>(
-                        <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
+                    filteredRestaurant.map((restaurant)=>(
+                        <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                     ))
                 }
             </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,15 +3,15 @@ import { CDN_URL } from "../utils/constants"
 const RestaurantCard = (props) =>{
     const {resData} = props
 
-    const {name , cuisines ,avgRating ,costForTwo, deliveryTime, cloudinaryImageId } = resData.data
+    const {name , cuisines ,avgRating ,costForTwo, sla, cloudinaryImageId } = resData.info
     return (
         <div className="res-card">
             <img alt="res-logo" className="res-logo" src={CDN_URL + cloudinaryImageId} />
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating} rating</h4>
-            <h4>₹{costForTwo / 100} FOR TWO</h4>
-            <h4>{deliveryTime} minutes</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{sla.deliveryTime} minutes</h4>
         </div>
     )
 }
