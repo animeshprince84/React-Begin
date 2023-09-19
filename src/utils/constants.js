7974feab2199a23f12afe1b015f3652d9982dd42
animeshprince84 <animeshprince84@gmail.com>
2023-09-19 13:03:11 +0530

    routing done and restaurant menu page created

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const LOGO_URL = "https://logo.com/image-cdn/images/kts928pd/production/b374a124fc505ab3255fadae257d90e8e4a4855e-449x432.png?w=1080&q=72";
+
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
